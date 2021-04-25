c54ba557263455a247ff75f59dc12903b452c38c
joe.miyamoto <joe.miyamoto@bitcoinbank.co.jp>
2021-04-25 17:11:47 +0900

    Add README and convenience scripts for running in local

---
+++ scripts/start_with_local_docker.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -e
+
+SCRIPT_DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+echo $SCRIPT_DIR
+
+admin_macaroon=$(echo ${SCRIPT_DIR}/../tests/NLoop.Server.Tests/data/lnd_user/chain/bitcoin/regtest/admin.macaroon)
+certthumbprint=$(openssl x509 -in ${SCRIPT_DIR}/../tests/NLoop.Server.Tests/data/lnd_user/tls.cert -noout -sha256 -fingerprint | cut -f 2 -d "=")
+
+dotnet run --project NLoop.Server -- --nohttps true --btc.lightningconnectionstring "type=lnd-rest;macaroonfilepath="$admin_macaroon";server=https://localhost:32736;certthumbprint="$certthumbprint""
