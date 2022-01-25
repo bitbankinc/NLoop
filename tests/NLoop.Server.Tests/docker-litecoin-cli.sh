93b437d42e78c1948a46eb890cb36d2ba54ad9c8
joe.miyamoto <joe.miyamoto@bitcoinbank.co.jp>
2022-01-25 18:02:04 +0900

    remove -T option from docker-compose scripts

--- tests/NLoop.Server.Tests/docker-litecoin-cli.sh
+++ tests/NLoop.Server.Tests/docker-litecoin-cli.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker-compose exec -T litecoind litecoin-cli -regtest -datadir="/data" -rpcport=43783 -rpcpassword=unsafepassword -rpcuser=johndoe "$@" | sed 's/\t//g'
+docker-compose exec litecoind litecoin-cli -regtest -datadir="/data" -rpcport=43783 -rpcpassword=unsafepassword -rpcuser=johndoe "$@"
--- tests/NLoop.Server.Tests/docker-lncli-user.sh
+++ tests/NLoop.Server.Tests/docker-lncli-user.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-docker-compose exec -T lnd_user lncli --tlscertpath=/data/tls.cert --macaroonpath=/data/admin.macaroon --rpcserver=localhost:32777 $@ | sed 's/\t//g'
+docker-compose exec lnd_user lncli --tlscertpath=/data/tls.cert --macaroonpath=/data/admin.macaroon --rpcserver=localhost:32777 $@
