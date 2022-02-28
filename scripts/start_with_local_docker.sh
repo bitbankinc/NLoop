78f7ebd4e6b9f6814ab43d6caad2d7653d6e3ba3
joe.miyamoto <joe.miyamoto@bitcoinbank.co.jp>
2022-02-28 22:35:14 +0900

    wip: support c_lightning

--- scripts/start_with_local_docker.sh
+++ scripts/start_with_local_docker.sh
@@ -2,6 +2,14 @@
 
 set -eu
 
+usage() {
+	echo "USAGE: ./start_with_local_docker.sh nodetype"
+  echo "nodetype is which lightning daemon implementation that local nloopd will connect to."
+  echo "must be either lnd or clightning, "
+	exit 1
+}
+[ "$1" != "clightning" -a "$1" != "lnd" ] && usage
+
 SCRIPT_DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 echo $SCRIPT_DIR
 
@@ -10,22 +18,26 @@ certthumbprint=$(openssl x509 -in ${SCRIPT_DIR}/../tests/NLoop.Server.Tests/data
 
 LOGGING__LogLevel__Microsoft=Information
 
-ASPNETCORE_ENVIRONMENT=Development dotnet run --project NLoop.Server -- \
-  --network regtest \
-  --nohttps true \
-  --btc.rpcuser=johndoe \
-  --btc.rpcpassword=unsafepassword \
-  --btc.rpchost=localhost \
-  --btc.rpcport=43782 \
-  --ltc.rpcuser=johndoe \
-  --ltc.rpcpassword=unsafepassword \
-  --ltc.rpchost=localhost \
-  --ltc.rpcport=43783 \
-  --lndgrpcserver https://localhost:32777 \
-  --lndmacaroonfilepath ${admin_macaroon} \
-  --lndcertthumbprint ${certthumbprint} \
-  --eventstoreurl tcp://admin:changeit@localhost:1113 \
-  --boltzhost http://localhost \
-  --boltzport 6028 \
-  --exchanges FTX
+if [ "$1" == "lnd" ]; then
+  ASPNETCORE_ENVIRONMENT=Development dotnet run --project NLoop.Server -- \
+    --network regtest \
+    --nohttps true \
+    --btc.rpcuser=johndoe \
+    --btc.rpcpassword=unsafepassword \
+    --btc.rpchost=localhost \
+    --btc.rpcport=43782 \
+    --ltc.rpcuser=johndoe \
+    --ltc.rpcpassword=unsafepassword \
+    --ltc.rpchost=localhost \
+    --ltc.rpcport=43783 \
+    --lndgrpcserver https://localhost:32777 \
+    --lndmacaroonfilepath ${admin_macaroon} \
+    --lndcertthumbprint ${certthumbprint} \
+    --eventstoreurl tcp://admin:changeit@localhost:1113 \
+    --boltzhost http://localhost \
+    --boltzport 6028 \
+    --exchanges FTX
 
+if [ "$1" == "clightning" ];then
+  echo "not ready"
+  exit 1
---
+++ tests/NLoop.Server.Tests/docker-lightning-cli-user.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+docker-compose exec -T clightning_user lightning-cli --network regtest --lightning-dir /data $@
+
