3a83fe32ab70d74b0fb091ab15575476fcc47400
joe.miyamoto <joe.miyamoto@bitcoinbank.co.jp>
2022-03-04 13:24:06 +0900

    update docker scripts for clightning

--- tests/NLoop.Server.Tests/cliutils/open_from_user_to_server.sh
+++ tests/NLoop.Server.Tests/cliutils/open_from_user_to_server.sh
@@ -1,5 +1,12 @@
-curl http://localhost:6028/getnodes | \
-  jq ".nodes.BTC.nodeKey" | \
-  xargs -I XX ./docker-lncli-user.sh openchannel --private XX 500000
+#!/usr/bin/env bash
+
+set -eu
+
+server_pubkey=$(curl http://localhost:6028/getnodes | jq ".nodes.BTC.nodeKey")
+
+./docker-lncli-user.sh openchannel --private $server_pubkey 500000
+
+feerate=$(./docker-lightning-cli-user.sh feerates perkw | jq ".perkw.opening")
+./docker-lightning-cli-user.sh fundchannel $server_pubkey 500000 $feerate false
 
 ./docker-bitcoin-cli.sh generatetoaddress 3 bcrt1qjwfqxekdas249pr9fgcpxzuhmndv6dqlulh44m
